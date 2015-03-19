9324e2e08a08bf4fb5e8f4d49793d70bba755402
wrathematics <wrathematics@gmail.com>
2015-03-18 20:53:35 -0400

    added demo

---
+++ demo/demo.r
@@ -0,0 +1,12 @@
+### How long has your system been up (seconds since boot)?
+Sys.uptime()
+cat("\n")
+
+### How long has this R session been running?
+Sys.runtime()
+cat("\n")
+
+### post-hot system.time()
+post.system.time()
+cat("\n")
+
